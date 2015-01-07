e3ca31384c3eaf60c2dc976fa0f60bf537ff8b32
James Tautges <jamesdtautges@gmail.com>
2015-01-06 19:40:33 -0600

    Added limit switches to the elevator subsystem.

--- src/org/usfirst/frc/team1306/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/Elevator.java
@@ -1,14 +1,22 @@
 package org.usfirst.frc.team1306.robot.subsystems;
 
+import org.usfirst.frc.team1306.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Elevator extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	
+	private final DigitalInput topLimit;
+	private final DigitalInput bottomLimit;
+	
+	public Elevator() {
+    	topLimit = new DigitalInput(RobotMap.ELEVATOR_TOP);
+    	bottomLimit = new DigitalInput(RobotMap.ELEVATOR_BOTTOM);
+	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
