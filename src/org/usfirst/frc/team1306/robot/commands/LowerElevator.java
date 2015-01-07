db14823a5d33e6ee60e37797f884a380a7427077
James Tautges <jamesdtautges@gmail.com>
2015-01-06 20:56:30 -0600

    Preliminary functionality for elevator lowering command

--- src/org/usfirst/frc/team1306/robot/commands/LowerElevator.java
+++ src/org/usfirst/frc/team1306/robot/commands/LowerElevator.java
@@ -1,36 +1,44 @@
 package org.usfirst.frc.team1306.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team1306.robot.OI;
 
 /**
  *
  */
-public class LowerElevator extends Command {
+public class LowerElevator extends CommandBase {
+	
+	private final OI oi;
 
     public LowerElevator() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        requires(elevator);
+        oi = new OI();
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	elevator.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	if(!elevator.hitBottom()) {
+    		elevator.down();
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return elevator.hitTop() || (oi.rightTrigger() == 0);
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	elevator.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	elevator.stop();
     }
 }
