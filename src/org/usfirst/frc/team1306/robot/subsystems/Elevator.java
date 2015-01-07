83a3db6de69a6c8c157b265e297c9496e7d10f0a
James Tautges <jamesdtautges@gmail.com>
2015-01-06 19:45:38 -0600

    Added empty methods for the future when we actually know how the elevator will work.

--- src/org/usfirst/frc/team1306/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/Elevator.java
@@ -19,8 +19,22 @@ public class Elevator extends Subsystem {
 	}
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void start() {
+    	
+    }
+    
+    public void stop() {
+    	
+    }
+    
+    public void raise() {
+    	
+    }
+    
+    public void lower() {
+    	
     }
 }
 
