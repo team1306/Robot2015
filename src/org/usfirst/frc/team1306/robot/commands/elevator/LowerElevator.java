7a73632b19da74f9e07f7e9f3c3d90f313d4c790
James Tautges <jamesdtautges@gmail.com>
2015-01-08 19:43:29 -0600

    Added package for elevator commands.

---
+++ src/org/usfirst/frc/team1306/robot/commands/elevator/LowerElevator.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team1306.robot.commands.elevator;
+
+import org.usfirst.frc.team1306.robot.OI;
+import org.usfirst.frc.team1306.robot.commands.CommandBase;
+
+/**
+ *
+ */
+public class LowerElevator extends CommandBase {
+	
+	private final OI oi;
+
+    public LowerElevator() {
+    	super("Lower Elevator");
+        requires(elevator);
+        oi = getOI();
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	elevator.start();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if(!elevator.hitBottom()) {
+    		elevator.down();
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return elevator.hitTop() || (oi.rightTrigger() == 0);
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	elevator.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	elevator.stop();
+    }
+}
