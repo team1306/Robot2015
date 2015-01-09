c9f2ad5a9986c36c26116ba54fe6fcd4966f6273
James Tautges <jamesdtautges@gmail.com>
2015-01-08 20:45:20 -0600

    Changed ports to objects.

--- src/org/usfirst/frc/team1306/robot/RobotMap.java
+++ src/org/usfirst/frc/team1306/robot/RobotMap.java
@@ -1,4 +1,10 @@
 package org.usfirst.frc.team1306.robot;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -17,23 +23,33 @@ public class RobotMap {
     // public static int rangefinderModule = 1;
 	
 	// Drive train motors
-	public static final int FRONT_LEFT = 3;
-	public static final int REAR_LEFT = 2;
-	public static final int FRONT_RIGHT = 1;
-	public static final int REAR_RIGHT = 0;
+	public static Talon DRIVE_FRONT_LEFT;
+	public static Talon DRIVE_REAR_LEFT;
+	public static Talon DRIVE_FRONT_RIGHT;
+	public static Talon DRIVE_REAR_RIGHT;
+	
+	public static RobotDrive DRIVETRAIN;
 	
 	// Misc inputs
-	public static final int ELEVATOR_BOTTOM = 0;
-	public static final int ELEVATOR_TOP = 1;
-	public static final int LEVEL_ONE = 2;
-	public static final int LEVEL_TWO = 3;
-	public static final int GYRO = 0;
+	public static DigitalInput ELEVATOR_BOTTOM_LIMIT;
+	public static DigitalInput ELEVATOR_TOP_LIMIT;
+	public static Gyro GYRO;
 	
 	// Manipulator actuators
-	public static final int ELEVATOR_MOTOR = 4;
+	public static Talon ELEVATOR_MOTOR;
 	
-	// Driver station
-	public static final int LEFT_STICK = 1;
-	public static final int RIGHT_STICK = 2;
-	public static final int XBOX_CONTROLLER = 0;
+	public static void init() {
+		DRIVE_FRONT_LEFT = new Talon(3);
+		DRIVE_REAR_LEFT = new Talon(2);
+		DRIVE_FRONT_RIGHT = new Talon(1);
+		DRIVE_REAR_RIGHT = new Talon(0);
+		
+		DRIVETRAIN = new RobotDrive(DRIVE_FRONT_LEFT, DRIVE_REAR_LEFT, DRIVE_FRONT_RIGHT, DRIVE_REAR_RIGHT);
+		
+		ELEVATOR_BOTTOM_LIMIT = new DigitalInput(0);
+		ELEVATOR_TOP_LIMIT = new DigitalInput(1);
+		GYRO = new Gyro(0);
+		
+		ELEVATOR_MOTOR = new Talon(4);
+	}
 }
