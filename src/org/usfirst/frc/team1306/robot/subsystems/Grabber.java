1bdbb629f83df4265c340e5e30a6ab9d34a911e2
James Tautges <jamesdtautges@gmail.com>
2015-01-10 13:28:16 -0600

    Added some empty methods for grabber while waiting for mech.

--- src/org/usfirst/frc/team1306/robot/subsystems/Grabber.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/Grabber.java
@@ -7,12 +7,28 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Grabber extends Subsystem {
     
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+    public Grabber() {
+    }
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void start() {
+    	
+    }
+    
+    public void stop() {
+    	
+    }
+    
+    public void open() {
+    	
+    }
+    
+    public void close() {
+    	
     }
 }
 
